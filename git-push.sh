0ff1e54ea7421a3438f3f3cd12a40b7292436295
Anton Skuratovich <iantonio.by@gmail.com>
2022-07-31 10:06:42 +0300

    Change script

--- git-push.sh
+++ git-push.sh
@@ -3,6 +3,6 @@ remotes=($(git remote)) #Put remote repo into array
 
 for i in ${!remotes[@]}; do
        echo "Push to ${remotes[$i]}"
-       git push ${remotes[$i]} --all
+       git push ${remotes[$i]}
        echo "-------------------------------" #delimiter
 done  
